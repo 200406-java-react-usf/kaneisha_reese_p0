522d029bd9e47d198f49e7116a23684df1099f72
KaneishaR <kaneisha.reese@gmail.com>
2020-04-22 23:43:13 -0500

    created groomers database

---
+++ src/data/groomers-db.ts
@@ -0,0 +1,11 @@
+import { Groomer } from '../models/groomers'
+
+let id = 1; 
+
+export default [
+    new Groomer(id++, "Suzie", "Whinchester", 0, 0),
+    new Groomer(id++, "Brandon", "Mann", 0, 0),
+    new Groomer(id++, "Paul", "Barton", 0, 0),
+    new Groomer(id++, "Lucy", "Thompson", 0, 0),
+    new Groomer(id++, "Andrew", "Whinchester", 0, 0)
+]
