f582e5fbfb07b8063b4f083537d6035819de9afb
KaneishaR <kaneisha.reese@gmail.com>
2020-05-01 08:57:30 -0500

    Updated animal model and db to match ERD

--- src/data/animals-db.ts
+++ src/data/animals-db.ts
@@ -3,7 +3,7 @@ import { Animal } from '../models/animals'
 let id = 1;
 
 export default [
-    new Animal(id++, 'Snickers', 1, 19, ['bath', 'style', 'nails', 'teeth']),
-    new Animal(id++, 'Reesey', 1, 65, ['bath', 'nails', 'teeth']),
-    new Animal(id++, 'Smoke', 1, 35, ['bath']),
+    new Animal(id++, 'Snickers', 1, 19, true),
+    new Animal(id++, 'Reesey', 1, 65, true),
+    new Animal(id++, 'Smoke', 1, 35, true),
 ]
--- src/models/animals.ts
+++ src/models/animals.ts
@@ -2,15 +2,28 @@ export class Animal{
 
     id: number;
     name: string;
-    ownerId: number; //foreign key to a different table
+    groomerId: number; //foreign key to a different table
     weight: number; // in lbs
-    services: string[];
+    status: boolean;
+    size: number;
 
-   constructor(id:number, name:string, owner:number, weight:number, services: string[]){
+
+   constructor(id:number, name:string, groomer:number, weight:number, status:boolean){
        this.id = id;
        this.name = name;
-       this.ownerId = owner;
+       this.groomerId = groomer;
        this.weight = weight;
-       this.services = services;
+       this.status = status;
+
+       if (weight < 15){
+           this.size = 1;
+       }
+        else if (weight < 30){
+           this.size = 2;
+       } 
+       else {
+           this.size = 3;
+        }
+
    }
 }
