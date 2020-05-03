72b670b24682c17eccfc4031670d25f66dc6ef56
KaneishaR <kaneisha.reese@gmail.com>
2020-05-02 21:11:30 -0500

    added groomers model and data

--- src/data/groomers-db.ts
+++ src/data/groomers-db.ts
@@ -3,9 +3,9 @@ import { Groomer } from '../models/groomer'
 let id = 1; 
 
 export default [
-    new Groomer(id++, "Suzie", "Whinchester", 0, 0),
-    new Groomer(id++, "Brandon", "Mann", 0, 0),
-    new Groomer(id++, "Paul", "Barton", 0, 0),
-    new Groomer(id++, "Lucy", "Thompson", 0, 0),
-    new Groomer(id++, "Andrew", "Whinchester", 0, 0)
+    new Groomer(id++,"swinchester", "password", "Suzie", "Whinchester", 0, 0),
+    new Groomer(id++, "bmann", "password","Brandon", "Mann", 0, 0),
+    new Groomer(id++, "pbarton", "password","Paul", "Barton", 0, 0),
+    new Groomer(id++, "lthompson", "password","Lucy", "Thompson", 0, 0),
+    new Groomer(id++, "awinchester", "password","Andrew", "Whinchester", 0, 0)
 ]
--- src/models/groomer.ts
+++ src/models/groomer.ts
@@ -1,19 +1,19 @@
 export class Groomer{
 
      id: number;
-     firstName: string;
-     lastName: string;
      username: string;
      password: string;
+     firstName: string;
+     lastName: string;
      hoursWorked: number;
      earnings: number;
 
-    constructor(id:number, fn:string, ln:string, hours:number, earn: number){
+    constructor(id:number, un: string, pass:string, fn:string, ln:string, hours:number, earn: number){
         this.id = id;
+        this.username = un;
+        this.password = pass;
         this.firstName = fn;
         this.lastName = ln;
-        this.username = `${ln}.${fn}`;
-        this.password = `${ln}${id}`;
         this.hoursWorked = hours;
         this.earnings = earn;
     }
