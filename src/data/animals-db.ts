df0db15bff7c9c116007438f579c5b96185a3321
KaneishaR <kaneisha.reese@gmail.com>
2020-05-06 05:31:47 -0500

    removed local animal db

--- src/data/animals-db.ts
+++
@@ -1,9 +0,0 @@
-import { Animal } from '../models/animal'
-
-let id = 1;
-
-export default [
-    new Animal(id++, 'Snickers', 1, 19, true),
-    new Animal(id++, 'Reesey', 1, 65, true),
-    new Animal(id++, 'Smoke', 1, 35, true),
-]
