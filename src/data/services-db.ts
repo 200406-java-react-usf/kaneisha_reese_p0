42a60be9c28d2c0d6ad1a4d3e9fb6cecb0889d2a
KaneishaR <kaneisha.reese@gmail.com>
2020-05-06 02:04:51 -0500

    removed services db

--- src/data/services-db.ts
+++
@@ -1,8 +0,0 @@
-import { Service } from '../models/service'
-
-export default [
-    new Service('bath', [0.5,0.5,1], [20, 25, 35]),
-    new Service('style', [1,1,1.5], [20, 25, 35]),
-    new Service('nails', [0.25,0.25,0.25], [10, 10, 10]),
-    new Service('teeth', [0.25,0.25,0.25], [10, 10, 10]),
-]
